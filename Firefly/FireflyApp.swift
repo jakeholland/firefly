b109df2a056195556fec446087c8f4fd51815d5d
Jake Holland <jakeholland@me.com>
2026-02-19 14:16:10 -0800

    Initial Commit

---
+++ Firefly/FireflyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FireflyApp.swift
+//  Firefly
+//
+//  Created by Jake Holland on 2/19/26.
+//
+
+import SwiftUI
+
+@main
+struct FireflyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
